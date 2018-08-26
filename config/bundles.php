a1c1352f017aa8ad3424ae2656d9791b304f7aa0
Ibrahim Tchee <ibrahim.chechen64@gmail.com>
2018-08-26 21:03:52 +0200

    JWTRefreshTokenBundle added and configured

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/api/test", name="default")
      */
     public function index()
     {
